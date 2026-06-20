b86fc638b1eae4fd1cc95dcff5bf7ae96255b567
ananyakh16-svg <ananyakh16@gmail.com>
2026-06-20 19:53:49 +0530

    Initial Portfolio

---
+++ ananya-portfolio/src/App.jsx
@@ -0,0 +1,73 @@
+function App() {
+
+  return (
+    <>
+    
+      <section className="hero">
+        <h1>Ananya KH</h1>
+        <p>B.Sc Data Science Student</p>
+        <p>AI • Data Science • Web Development</p>
+      </section>
+
+      <section>
+        <h2>About Me</h2>
+        <p>
+          I am a B.Sc Data Science student at St. Thomas College Thrissur.
+          Passionate about AI, Data Science and building impactful
+          technology solutions.
+        </p>
+      </section>
+
+      <section>
+        <h2>Skills</h2>
+
+        <div className="skills">
+          <span>Python</span>
+          <span>C</span>
+          <span>HTML</span>
+          <span>CSS</span>
+          <span>JavaScript</span>
+          <span>React</span>
+          <span>Django</span>
+          <span>Flutter</span>
+          <span>MySQL</span>
+          <span>MongoDB</span>
+        </div>
+      </section>
+
+      <section>
+        <h2>Projects</h2>
+
+        <div className="card">
+          <h3>Club management system</h3>
+          <p>Developed a full stack club management system using React and Django to streamline club operations and improve member engagement.</p>
+        </div>
+      </section>
+
+      <section>
+        <h2>Certificates</h2>
+
+        <ul>
+          <li>Python 101 For Data Science</li>
+          <li>Sql And Relational Databases 101</li>
+          <li>Introduction To Open Source</li>
+          <li>Data Science Tools</li>
+          <li>Data Privacy Fundamentals</li>
+          <li>Data Science Methodology</li>
+          <li>Data Science 101</li>        </ul>
+      </section>
+
+      <section>
+        <h2>Contact</h2>
+
+        <p>Email: ananyakh16@gmail.com</p>
+        <p>LinkedIn:https://www.linkedin.com/in/ananya-kh-30004a373/ </p>
+        <p>GitHub: https://github.com/ananyakh16-svg/New-coding-</p>
+      </section>
+
+    </>
+      
+  );
+}
+
+export default App
